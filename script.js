e21f7e93896d76ce402d7c6d7698d4e82da160ac
f23949288-blip <f23949288@gmail.com>
2026-07-26 12:05:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,86 @@
 
+// Scene
+const scene = new THREE.Scene();
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+75,
+window.innerWidth/window.innerHeight,
+0.1,
+1000
+);
+
+camera.position.z = 5;
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+alpha:true,
+antialias:true
+});
+
+renderer.setSize(window.innerWidth,window.innerHeight);
+
+renderer.domElement.style.position="fixed";
+renderer.domElement.style.top="0";
+renderer.domElement.style.left="0";
+renderer.domElement.style.zIndex="-1";
+
+document.body.appendChild(renderer.domElement);
+
+// Stars
+const geometry = new THREE.BufferGeometry();
+const vertices=[];
+
+for(let i=0;i<4000;i++){
+
+vertices.push(
+(Math.random()-0.5)*200,
+(Math.random()-0.5)*200,
+(Math.random()-0.5)*200
+);
+
+}
+
+geometry.setAttribute(
+'position',
+new THREE.Float32BufferAttribute(vertices,3)
+);
+
+const material=new THREE.PointsMaterial({
+color:0xffd700,
+size:0.4
+});
+
+const stars=new THREE.Points(
+geometry,
+material
+);
+
+scene.add(stars);
+
+// Animation
+
+function animate(){
+
+requestAnimationFrame(animate);
+
+stars.rotation.y+=0.0008;
+stars.rotation.x+=0.0002;
+
+renderer.render(scene,camera);
+
+}
+
+animate();
+
+// Resize
+
+window.addEventListener("resize",()=>{
+
+camera.aspect=window.innerWidth/window.innerHeight;
+
+camera.updateProjectionMatrix();
+
+renderer.setSize(window.innerWidth,window.innerHeight);
+
+});
